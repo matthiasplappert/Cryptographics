7bb0406a60bfec8b57b7c3ae8b1d8dde400a9b98
Matthias Plappert <matthiasplappert@me.com>
2013-12-04 16:58:59 +0100

    Add AbstractController methods.

--- cg_implementierung/src/edu/kit/iks/CryptograhicsLib/AbstractController.java
+++ cg_implementierung/src/edu/kit/iks/CryptograhicsLib/AbstractController.java
@@ -1,5 +1,33 @@
 package edu.kit.iks.CryptograhicsLib;
 
-public class AbstractController {
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JComponent;
 
+public class AbstractController {
+	public JComponent view = null;
+	
+	public AbstractController parentController = null;
+	
+	public List<AbstractController> childControllers;
+	
+	public AbstractController() {
+		this.childControllers = new ArrayList<AbstractController>();
+	}
+	
+	public void addChildController(AbstractController childController) {
+		
+	}
+	
+	public void removeChildController(AbstractController childController) {
+		
+	}
+	
+	public void loadView() {
+		
+	}
+	
+	public void unloadView() {
+		this.view = null;
+	}
 }
