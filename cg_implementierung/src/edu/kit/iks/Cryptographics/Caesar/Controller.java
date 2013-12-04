e8d97b3215ceb2b0859018de4c026a848a32532b
Matthias Plappert <matthiasplappert@me.com>
2013-12-04 19:33:15 +0100

    Add necessary methods to abstract viusalization controller.

--- cg_implementierung/src/edu/kit/iks/Cryptographics/Caesar/Controller.java
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/Caesar/Controller.java
@@ -1,7 +1,10 @@
 package edu.kit.iks.Cryptographics.Caesar;
 
 import edu.kit.iks.Cryptographics.AbstractVisualizationController;
+import edu.kit.iks.Cryptographics.AbstractVisualizationInfo;
 
 public class Controller extends AbstractVisualizationController {
-
+	public Controller(AbstractVisualizationInfo visualizationInfo) {
+		super(visualizationInfo);
+	}
 }
