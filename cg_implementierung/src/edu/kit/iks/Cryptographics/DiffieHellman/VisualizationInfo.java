429cd61b58fe4842268152dac7a51f52b888a484
Matthias Plappert <matthiasplappert@me.com>
2013-12-04 18:02:19 +0100

    Add DiffieHellman and Vigenere packages.

---
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/DiffieHellman/VisualizationInfo.java
@@ -0,0 +1,7 @@
+package edu.kit.iks.Cryptographics.DiffieHellman;
+
+import edu.kit.iks.Cryptographics.AbstractVisualizationInfo;
+
+public class VisualizationInfo extends AbstractVisualizationInfo {
+
+}
---
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/Vigenere/VisualizationInfo.java
@@ -0,0 +1,7 @@
+package edu.kit.iks.Cryptographics.Vigenere;
+
+import edu.kit.iks.Cryptographics.AbstractVisualizationInfo;
+
+public class VisualizationInfo extends AbstractVisualizationInfo {
+
+}
