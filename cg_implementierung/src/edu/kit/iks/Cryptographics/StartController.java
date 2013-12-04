cd11ffa7fd3d54409e1f20627dbf283bcd5cd48e
Matthias Plappert <matthiasplappert@me.com>
2013-12-04 19:06:27 +0100

    Add StartController methods and related classes.

--- cg_implementierung/src/edu/kit/iks/Cryptographics/StartController.java
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/StartController.java
@@ -3,6 +3,24 @@ package edu.kit.iks.Cryptographics;
 import edu.kit.iks.CryptograhicsLib.AbstractController;
 
 public class StartController extends AbstractController {
+	private PopoverView _popoverView;
+	
+	private TimelineView _timelineView;
+	
+	private WelcomeView _welcomeView;
+	
+	public void presentPopoverAction(AbstractVisualizationInfo visualizationInfo) {
+		
+	}
+	
+	public void dismissPopoverAction() {
+		
+	}
+	
+	public void startVisualizationAction(AbstractVisualizationInfo visualizationInfo) {
+		
+	}
+	
 	private void _loadAllVisualizationInfos() {
 		
 	}
