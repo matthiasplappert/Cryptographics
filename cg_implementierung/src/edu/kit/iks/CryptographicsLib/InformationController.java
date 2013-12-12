8752c0b25d25911fe978e78c49fa2912da86d09d
Matthias Plappert <matthiasplappert@me.com>
2013-12-12 18:17:18 +0100

    Move InformationController to lib.

--- cg_implementierung/src/edu/kit/iks/CryptographicsLib/InformationController.java
+++ cg_implementierung/src/edu/kit/iks/CryptographicsLib/InformationController.java
@@ -1,7 +1,5 @@
-package edu.kit.iks.Cryptographics;
+package edu.kit.iks.CryptographicsLib;
 
-import edu.kit.iks.CryptographicsLib.AbstractVisualizationController;
-import edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo;
 
 public class InformationController extends AbstractVisualizationController {
 	public InformationController(AbstractVisualizationInfo visualizationInfo) {
