313b9e054d63b5ac6050bdffabe69e366b59f06b
Matthias Plappert <matthiasplappert@me.com>
2013-12-12 18:11:27 +0100

    Add skeletons for concrete controllers.

---
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/DiffieHellman/DemonstrationController.java
@@ -0,0 +1,11 @@
+package edu.kit.iks.Cryptographics.DiffieHellman;
+
+import edu.kit.iks.CryptographicsLib.AbstractVisualizationController;
+
+public class DemonstrationController extends AbstractVisualizationController {
+	private VisualizationInfo _visualizationInfo;
+	
+	public DemonstrationController(VisualizationInfo visualizationInfo) {
+		super(visualizationInfo);
+	}
+}
---
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/InformationController.java
@@ -0,0 +1,10 @@
+package edu.kit.iks.Cryptographics;
+
+import edu.kit.iks.CryptographicsLib.AbstractVisualizationController;
+import edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo;
+
+public class InformationController extends AbstractVisualizationController {
+	public InformationController(AbstractVisualizationInfo visualizationInfo) {
+		super(visualizationInfo);
+	}
+}
