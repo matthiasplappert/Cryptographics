36471cdc73693086d85828c944ff423c33ec9e49
Christian Dreher <CDreher3011@gmail.com>
2013-12-12 17:21:03 +0100

    * Small changes

--- cg_implementierung/src/edu/kit/iks/Cryptographics/VisualizationContainerController.java
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/VisualizationContainerController.java
@@ -7,9 +7,14 @@ import edu.kit.iks.CryptographicsLib.AbstractVisualizationController;
 import edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo;
 
 public class VisualizationContainerController extends AbstractController {
-	private VisualizationContainerView _containerView;
 	
+	/**
+	 * Active child controller. 
+	 * Overrides {_activeChildController} from {AbstractController}
+	 */
 	protected AbstractVisualizationController _activeChildController;
+	
+	private VisualizationContainerView _containerView;
 	
 	private AbstractVisualizationInfo _visualizationInfo;
 	
@@ -18,7 +23,9 @@ public class VisualizationContainerController extends AbstractController {
 		
 		// TODO: Example to instantiate all possible child classes
 		List<Class> childClasses = this._visualizationInfo.getControllerClasses();
+		
 		for (Class<AbstractVisualizationController> childClass : childClasses) {
+			
 			AbstractVisualizationController controller = null;
 			
 			try {
