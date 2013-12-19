834b5d7a32fda99b36d1849572010133fb65ccc7
Christian Dreher <CDreher3011@gmail.com>
2013-12-19 17:08:34 +0100

    + Added Javadoc of TimelineView

--- cg_implementierung/src/edu/kit/iks/Cryptographics/TimelineView.java
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/TimelineView.java
@@ -2,6 +2,11 @@ package edu.kit.iks.Cryptographics;
 
 import javax.swing.JPanel;
 
+/**
+ * An instance of this class represents the view of a timeline 
+ * 
+ * @author Christian Dreher
+ */
 public class TimelineView extends JPanel {
 
 }
