928cbac0c3736433f15425f16a7951ec73fa8a40
Christian Dreher <CDreher3011@gmail.com>
2013-12-19 17:28:20 +0100

    + Added Javadoc of VisualizationContainerView

--- cg_implementierung/src/edu/kit/iks/Cryptographics/VisualizationContainerView.java
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/VisualizationContainerView.java
@@ -4,10 +4,27 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+/**
+ * An instance of this class represents the view of container controller
+ * beside the actual visualization
+ * 
+ * @author Christian Dreher
+ */
 public class VisualizationContainerView extends JPanel {
+	
+	/**
+	 * Name of the current visualization
+	 */
 	private JLabel _nameLabel;
 	
+	/**
+	 * Button to exit the current visualization
+	 */
 	private JButton _exitButton;
 	
+	/**
+	 * Button to access interactive help for the current step inside the
+	 * visualization
+	 */
 	private JButton _helpButton;
 }
