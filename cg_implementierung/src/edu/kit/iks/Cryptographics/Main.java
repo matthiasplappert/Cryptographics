2a7bb4ba93cc9084beb9c9c48043085061440ba3
Christian Dreher <CDreher3011@gmail.com>
2013-12-19 15:48:24 +0100

    + Added Javadoc for Main

--- cg_implementierung/src/edu/kit/iks/Cryptographics/Main.java
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/Main.java
@@ -1,6 +1,17 @@
 package edu.kit.iks.Cryptographics;
 
+/**
+ * Main Class containing the main()-method
+ * 
+ * @author Christian Dreher
+ */
 public class Main {
+	
+	/**
+	 * Main method (hook for Java to start from)
+	 * 
+	 * @param args  Arguments
+	 */
 	public static void main(String[] args) {
 		MainController mainController = new MainController();
 		mainController.loadView();
