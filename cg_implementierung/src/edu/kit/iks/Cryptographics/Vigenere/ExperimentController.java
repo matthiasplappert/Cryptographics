7cf4fa6bfd1c404268be63ceb74a089a80514a3b
AydinTekin <tekin.aydin.66@googlemail.com>
2013-12-20 13:06:04 +0100

    several fixes

--- cg_implementierung/src/edu/kit/iks/Cryptographics/Vigenere/ExperimentController.java
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/Vigenere/ExperimentController.java
@@ -3,8 +3,6 @@ package edu.kit.iks.Cryptographics.Vigenere;
 import edu.kit.iks.CryptographicsLib.AbstractVisualizationController;
 
 public class ExperimentController extends AbstractVisualizationController {
-	private VisualizationInfo _visualizationInfo;
-	
 	public ExperimentController(VisualizationInfo visualizationInfo) {
 	}
 }
