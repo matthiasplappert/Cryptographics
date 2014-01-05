2299c9b92fa75bddcb76a9c18b43fc4489defccf
Matthias Plappert <matthiasplappert@me.com>
2014-01-05 16:26:08 +0100

    Fix DiffieHellman controllers.

--- cg_implementierung/src/edu/kit/iks/Cryptographics/DiffieHellman/DemonstrationController.java
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/DiffieHellman/DemonstrationController.java
@@ -7,36 +7,32 @@ import javax.swing.JPanel;
 import edu.kit.iks.Cryptographics.DiffieHellman.State.DHFirstState;
 import edu.kit.iks.Cryptographics.DiffieHellman.State.DHStateController;
 import edu.kit.iks.CryptographicsLib.AbstractVisualizationController;
+import edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo;
 
 public class DemonstrationController extends AbstractVisualizationController {
-	private JPanel view;
+	private Model model;
 	
-	private DHStateController state;
-	
-	private DiffieHellmanModel model;
-	
-	private VisualizationInfo _visualizationInfo;
-	
-	public DemonstrationController() {
-		super();
-		this._visualizationInfo = new VisualizationInfo();
-		this.state = new DHFirstState();
-		this.view = this.state.getView();
-		this.model = new DiffieHellmanModel();
-	}
-	
-	public JPanel getView() {
-		return this.view;
+	public DemonstrationController(AbstractVisualizationInfo visualizationInfo) {
+		super(visualizationInfo);
+		this.model = new Model();
 	}
 	
 	public void nextStateListener(ActionEvent event) {
-		//push(state)
-		this.state = this.state.nextState();
-		this.view = this.state.getView();
+		// TODO go to next state
 	}
 	
-	public void prevStateListener(ActionEvent event) {
-		this.state = this.state.previousState();
-		this.view = this.state.getView();
+	public void previousStateListener(ActionEvent event) {
+		// TODO go to previous state
+	}
+
+	@Override
+	public String getHelp() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void loadView() {
+		// TODO Auto-generated method stub
 	}
 }
--- cg_implementierung/src/edu/kit/iks/Cryptographics/DiffieHellman/VisualizationInfo.java
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/DiffieHellman/VisualizationInfo.java
@@ -2,36 +2,48 @@ package edu.kit.iks.Cryptographics.DiffieHellman;
 
 import java.util.List;
 
-import edu.kit.iks.CryptographicsLib.AbstractVisualizationController;
 import edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo;
 import edu.kit.iks.CryptographicsLib.VisualizationDifficulty;
 
 public class VisualizationInfo extends AbstractVisualizationInfo {
+	@Override
 	public String getId() {
 		return "diffiehellman";
 	}
 	
+	@Override
 	public String getName() {
 		return "Diffie-Hellman";
 	}
 	
+	@Override
 	public String getDescription() {
 		return "";
 	}
 
+	@Override
 	public float getTimelineOffset() {
 		return 0.0f;
 	}
 
+	@Override
 	public VisualizationDifficulty getDifficulty() {
 		return VisualizationDifficulty.EASY;
 	}
 
+	@Override
 	public int getYear() {
 		return 1976;
 	}
 	
+	@Override
 	public List<Class> getControllerClasses() {
+		return null;
+	}
+
+	@Override
+	public String getQRCodeContent() {
+		// TODO Auto-generated method stub
 		return null;
 	}
 }
