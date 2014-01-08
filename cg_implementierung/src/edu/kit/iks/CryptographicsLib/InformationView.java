232003cbf5287017633b5c35af32e0d871165ff2
Christian Dreher <CDreher3011@gmail.com>
2014-01-08 17:59:32 +0100

    + Add missing javadoc in InformationView.java

--- cg_implementierung/src/edu/kit/iks/CryptographicsLib/InformationView.java
+++ cg_implementierung/src/edu/kit/iks/CryptographicsLib/InformationView.java
@@ -5,25 +5,54 @@ import java.net.URL;
 
 import javax.swing.JPanel;
 
+/**
+ * View of the information page
+ * 
+ * @author Christian Dreher
+ */
 public class InformationView extends JPanel {
+	
 	/**
-	 * 
+	 * Serial Version UID
 	 */
 	private static final long serialVersionUID = -8593552609491347799L;
 
+	/**
+	 * QR code for further information
+	 */
 	private Image qrCode;
 	
+	/**
+	 * File path to the local HTML file with further information
+	 */
 	private URL path;
 	
+	/**
+	 * Constructor initializing a new instance of {InformationView}
+	 * with given {path} and {qrCode}
+	 * 
+	 * @param path File path to the local HTML file with further information
+	 * @param qrCode QR code for further information
+	 */
 	public InformationView(URL path, Image qrCode) {
 		this.qrCode = qrCode;
 		this.path = path;
 	}
 	
+	/**
+	 * Gets the file path to the local HTML file with further information
+	 * 
+	 * @return File path to the local HTML file with further information
+	 */
 	public URL getPath() {
 		return this.path;
 	}
 	
+	/**
+	 * Gets the QR code for further information
+	 * 
+	 * @return QR code for further information
+	 */
 	public Image getQrCode() {
 		return this.qrCode;
 	}
