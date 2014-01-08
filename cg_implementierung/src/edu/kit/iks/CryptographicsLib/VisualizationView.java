1cb2c59c618f596b2a0fd18d9b5750705b202e50
Christian Dreher <CDreher3011@gmail.com>
2014-01-08 17:59:42 +0100

    + Add missing javadoc in VisualizationView.java

--- cg_implementierung/src/edu/kit/iks/CryptographicsLib/VisualizationView.java
+++ cg_implementierung/src/edu/kit/iks/CryptographicsLib/VisualizationView.java
@@ -3,20 +3,42 @@ package edu.kit.iks.CryptographicsLib;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
+/**
+ * View for UI elements which all visualizations have in common
+ * 
+ * @author Christian Dreher
+ */
 public class VisualizationView extends JPanel {
+	
 	/**
-	 * 
+	 * Serial Version UID 
 	 */
 	private static final long serialVersionUID = -988624050394454370L;
 
+	/**
+	 * Next button
+	 */
 	private JButton nextButton;
 	
+	/**
+	 * Back button
+	 */
 	private JButton backButton;
 	
+	/**
+	 * Gets the next button
+	 * 
+	 * @return The next button
+	 */
 	public JButton getNextButton() {
 		return nextButton;
 	}
 	
+	/**
+	 * Gets the back button
+	 * 
+	 * @return The back button
+	 */
 	public JButton getBackButton() {
 		return backButton;
 	}
