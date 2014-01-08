c8a651914fa3a862b336b9780304845fe28360fb
Matthias Jaenicke <matthias.jaenicke@student.kit.edu>
2014-01-08 17:18:24 +0100

    Changed Forward/BackButton to CustomButton

---
+++ cg_implementierung/src/edu/kit/iks/CryptographicsLib/UICustomButton.java
@@ -0,0 +1,14 @@
+package edu.kit.iks.CryptographicsLib;
+
+import javax.swing.JButton;
+
+/**
+ * @author Matthias Jaenicke.
+ * 
+ */
+public class UICustomButton extends JButton {
+
+	public UICustomButton() {
+		super();
+	}
+}
