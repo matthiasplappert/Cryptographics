4f701ee4242c897b1109c2c60cbf83126dae8f6c
Matthias Jaenicke <matthias.jaenicke@student.kit.edu>
2014-01-08 17:10:21 +0100

    Created first batch of UI elements
    UI elements currently known to be needed (Caesar, Vigenere) have been
    created.

---
+++ cg_implementierung/src/edu/kit/iks/CryptographicsLib/UIKeyboardKey.java
@@ -0,0 +1,27 @@
+package edu.kit.iks.CryptographicsLib;
+
+import javax.swing.JButton;
+
+/**
+ * 
+ * @author Matthias Jaenicke
+ *
+ */
+public class UIKeyboardKey extends JButton{
+
+	private char keyValue;
+	
+	public UIKeyboardKey(char keyValue) {
+		super();
+		
+		setKeyValue(keyValue);
+	}
+
+	public char getKeyValue() {
+		return keyValue;
+	}
+
+	public void setKeyValue(char keyValue) {
+		this.keyValue = keyValue;
+	}
+}
