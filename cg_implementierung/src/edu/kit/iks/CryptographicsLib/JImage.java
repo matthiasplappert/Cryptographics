e6ad20f9402af043a459a77f26ac2955f9a11d66
AydinTekin <tekin.aydin.66@googlemail.com>
2014-01-08 15:29:34 +0100

    JImage fixed, now accepts File AND Image

--- cg_implementierung/src/edu/kit/iks/CryptographicsLib/JImage.java
+++ cg_implementierung/src/edu/kit/iks/CryptographicsLib/JImage.java
@@ -1,6 +1,8 @@
 package edu.kit.iks.CryptographicsLib;
 
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -9,14 +11,31 @@ import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class JImage extends JPanel{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private BufferedImage image;
 
-    public JImage() {
-       try {                
-          image = ImageIO.read(new File("image name and path"));
-       } catch (IOException ex) {
-            // handle exception...
-       }
+    public JImage(Image img) {
+		if (img instanceof BufferedImage)
+	    {
+	        this.image = (BufferedImage) img;
+	    }
+	    // Create a buffered image with transparency
+	    BufferedImage bimage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+	    // Draw the image on to the buffered image
+	    Graphics2D bGr = bimage.createGraphics();
+	    bGr.drawImage(img, 0, 0, null);
+	    bGr.dispose();
+	    this.image = bimage;
+    }
+    
+    public JImage(String filePath) {
+    	try {                
+            image = ImageIO.read(new File(filePath));
+         } catch (IOException ex) {
+         }
     }
 
     @Override
