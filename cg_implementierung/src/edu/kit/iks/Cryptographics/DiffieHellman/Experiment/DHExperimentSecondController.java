9d04213ae9eea8d35983a4494f033d8a62ebf41e
kai-rcc <kai-rcc@gmx.de>
2014-01-09 15:46:21 +0100

    use the right model in the dh-controller and instantiate the right views

--- cg_implementierung/src/edu/kit/iks/Cryptographics/DiffieHellman/Experiment/DHExperimentSecondController.java
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/DiffieHellman/Experiment/DHExperimentSecondController.java
@@ -1,13 +1,18 @@
 package edu.kit.iks.Cryptographics.DiffieHellman.Experiment;
 
+import edu.kit.iks.Cryptographics.DiffieHellman.Model;
 import edu.kit.iks.CryptographicsLib.AbstractVisualizationController;
 import edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo;
 
 public class DHExperimentSecondController extends AbstractVisualizationController {
-
+	private Model dhModel;
+	private DHExperimentSecondView view;
+	
 	public DHExperimentSecondController(
 			AbstractVisualizationInfo visualizationInfo) {
 		super(visualizationInfo);
+		dhModel = Model.getInstance();
+		view = new DHExperimentSecondView();
 		// TODO Auto-generated constructor stub
 	}
 
--- cg_implementierung/src/edu/kit/iks/Cryptographics/DiffieHellman/Experiment/DHExperimentThirdController.java
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/DiffieHellman/Experiment/DHExperimentThirdController.java
@@ -1,13 +1,18 @@
 package edu.kit.iks.Cryptographics.DiffieHellman.Experiment;
 
+import edu.kit.iks.Cryptographics.DiffieHellman.Model;
 import edu.kit.iks.CryptographicsLib.AbstractVisualizationController;
 import edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo;
 
 public class DHExperimentThirdController extends AbstractVisualizationController {
-
+	private Model dhModel;
+	private DHExperimentThirdView view;
+	
 	public DHExperimentThirdController(
 			AbstractVisualizationInfo visualizationInfo) {
 		super(visualizationInfo);
+		dhModel = Model.getInstance();
+		view = new DHExperimentThirdView();
 		// TODO Auto-generated constructor stub
 	}
 
