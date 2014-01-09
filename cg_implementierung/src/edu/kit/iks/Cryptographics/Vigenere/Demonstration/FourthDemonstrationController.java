2e1552d389b00a4eae08893a1865aceaadb926fd
AydinTekin <tekin.aydin.66@googlemail.com>
2014-01-09 17:24:52 +0100

    Finished Vigenere.Experiment

--- cg_implementierung/src/edu/kit/iks/Cryptographics/Vigenere/Demonstration/FourthDemonstrationController.java
+++
@@ -1,9 +0,0 @@
-package edu.kit.iks.Cryptographics.Vigenere.Demonstration;
-
-import edu.kit.iks.Cryptographics.Vigenere.*;
-
-public class FourthDemonstrationController extends AbstractController {
-	public FourthDemonstrationController(VigenereVisualizationInfo visualizationInfo) {
-		super(visualizationInfo);
-	}
-}
