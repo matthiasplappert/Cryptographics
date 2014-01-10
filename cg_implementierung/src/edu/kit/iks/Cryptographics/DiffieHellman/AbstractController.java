e2870a18b17e74c9e540ab79d52a23e891dcc33f
Matthias Plappert <matthiasplappert@me.com>
2014-01-10 08:25:20 +0100

    Update images.

--- cg_implementierung/src/edu/kit/iks/Cryptographics/DiffieHellman/AbstractController.java
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/DiffieHellman/AbstractController.java
@@ -3,7 +3,7 @@ package edu.kit.iks.Cryptographics.DiffieHellman;
 import edu.kit.iks.CryptographicsLib.AbstractVisualizationController;
 import edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo;
 
-public class AbstractController extends AbstractVisualizationController {
+abstract public class AbstractController extends AbstractVisualizationController {
 	
 	private Model model;
 
@@ -15,17 +15,5 @@ public class AbstractController extends AbstractVisualizationController {
 	public Model getModel() {
 		return this.model;
 	}
-
-	@Override
-	public String getHelp() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void loadView() {
-		// TODO Auto-generated method stub
-
-	}
-
+	
 }
