e9de1effb438b4ef9fecd5934b28c5576e636081
Matthias Jaenicke <matthias.jaenicke@student.kit.edu>
2014-01-16 18:33:57 +0100

    Some implementations for UI elemtens

--- cg_implementierung/src/edu/kit/iks/CryptographicsLib/AlphabetStripView.java
+++ cg_implementierung/src/edu/kit/iks/CryptographicsLib/AlphabetStripView.java
@@ -1,10 +1,11 @@
 package edu.kit.iks.CryptographicsLib;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SpringLayout;
 
 /**
  * @author Matthias Jaenicke.
@@ -13,25 +14,34 @@ import javax.swing.SpringLayout;
  * Beneath it is matched by a corresponding strip of the numbers from 1 to 26.
  */
 public class AlphabetStripView extends JPanel {
-	private static JLabel[] strip;
+	private static JPanel strip;
+	private static NumbersStripView numberStrip;
 	
 	public AlphabetStripView() {
 		
 		super();
-		SpringLayout layout = new SpringLayout();
+		BorderLayout layout = new BorderLayout();
 		this.setLayout(layout);
 		
-		strip = new JLabel[26];
+		FlowLayout stripLayout = new FlowLayout();
+		strip = new JPanel(); //new JLabel[26];
+		strip.setLayout(stripLayout);
+		
+		numberStrip = new NumbersStripView();
 
 		int asciiA = 65;
+		JLabel currChar;
 		for (int i = asciiA; i < asciiA+26; i++) {
-			strip[i] = new JLabel(String.valueOf((char)i));
-			strip[i].setVerticalAlignment(JLabel.CENTER);
-			strip[i].setHorizontalAlignment(JLabel.CENTER);
-			this.add(strip[i]);
+			currChar = new JLabel(String.valueOf((char)i));
+			currChar.setVerticalAlignment(JLabel.CENTER);
+			currChar.setHorizontalAlignment(JLabel.CENTER);
+			strip.add(currChar);
 		}
 		
-		Dimension d = new Dimension(20 * strip.length, 20);
+		this.add(strip, BorderLayout.NORTH);
+		this.add(numberStrip, BorderLayout.SOUTH);
+		
+		Dimension d = new Dimension(20 * 26, 20);
 		this.setPreferredSize(d);
 	}
 }
--- cg_implementierung/src/edu/kit/iks/CryptographicsLib/KeyboardView.java
+++ cg_implementierung/src/edu/kit/iks/CryptographicsLib/KeyboardView.java
@@ -1,5 +1,12 @@
 package edu.kit.iks.CryptographicsLib;
 
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 /**
@@ -9,31 +16,72 @@ import javax.swing.JPanel;
  * Contains only the basic 26 capital characters.
  * Consists of UIKeyboardKey instances.
  */
-public class KeyboardView extends JPanel{
+public class KeyboardView extends JPanel implements ActionListener{
 
-	private static KeyboardButton[] keysFirstRow;
-	private static KeyboardButton[] keysSecondRow;
-	private static KeyboardButton[] keysThirdRow;
+	private JPanel firstRow;
+	private JPanel secondRow;
+	private JPanel thirdRow;
+	
+	private static JButton[] keysFirstRow;
+	private static JButton[] keysSecondRow;
+	private static JButton[] keysThirdRow;
 	
 	public KeyboardView() {
 		super();
 		
-		char[] charactersFirstRow = {'Q', 'W', 'E', 'R', 'T', 'Z', 'U', 'I', 'O', 'P'};
-		char[] charactersSecondRow = {'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'};
-		char[] charactersThirdRow = {'Y', 'X', 'C', 'V', 'B', 'N', 'M'};
-		keysFirstRow = new KeyboardButton[charactersFirstRow.length];
-		keysSecondRow = new KeyboardButton[charactersSecondRow.length];
-		keysThirdRow = new KeyboardButton[charactersThirdRow.length];
+		BoxLayout layout = new BoxLayout(this, BoxLayout.Y_AXIS);
+		this.setLayout(layout);
+		
+		
+		FlowLayout layoutRows = new FlowLayout();
+		JPanel firstRow = new JPanel();
+		firstRow.setLayout(layoutRows);
+		JPanel secondRow = new JPanel();
+		secondRow.setLayout(layoutRows);
+		JPanel thirdRow = new JPanel();
+		thirdRow.setLayout(layoutRows);
+		
+		String[] charactersFirstRow = {"Q", "W", "E", "R", "T", "Z", "U", "I", "O", "P", "Back"};
+		String[] charactersSecondRow = {"A", "S", "D", "F", "G", "H", "J", "K", "L", "Enter"};
+		String[] charactersThirdRow = {"Y", "X", "C", "V", "B", "N", "M"};
+//		Dimension keySize = new Dimension(47, 25);
+		keysFirstRow = new JButton[charactersFirstRow.length];
+		keysSecondRow = new JButton[charactersSecondRow.length];
+		keysThirdRow = new JButton[charactersThirdRow.length];
 		
 		for (int i = 0; i < charactersFirstRow.length; i++) {
-			keysFirstRow[i] = new KeyboardButton(charactersFirstRow[i]);
+			keysFirstRow[i] = new JButton(charactersFirstRow[i]);
+//			keysFirstRow[i].setPreferredSize(keySize);
+			keysFirstRow[i].addActionListener(this);
+			firstRow.add(keysFirstRow[i]);
+			
 		}
+		this.add(firstRow);
+		
 		for (int i = 0; i < charactersSecondRow.length; i++) {
-			keysSecondRow[i] = new KeyboardButton(charactersSecondRow[i]);
+			keysSecondRow[i] = new JButton(charactersSecondRow[i]);
+//			keysSecondRow[i].setPreferredSize(keySize);
+			keysSecondRow[i].addActionListener(this);
+			secondRow.add(keysSecondRow[i]);
 		}
+		this.add(secondRow);
+		
 		for (int i = 0; i < charactersThirdRow.length; i++) {
-			keysThirdRow[i] = new KeyboardButton(charactersThirdRow[i]);
+			keysThirdRow[i] = new JButton(charactersThirdRow[i]);
+//			keysThirdRow[i].setPreferredSize(keySize);
+			keysThirdRow[i].addActionListener(this);
+			thirdRow.add(keysThirdRow[i]);
 		}
+		this.add(thirdRow);
+		
+		this.setSize(firstRow.getWidth(), firstRow.getHeight() * 3);
+		this.setVisible(true);
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
 		
 	}
 }
