eb235f5dff7da9cf16144fc421bacf0713c433d5
Wasilij Beskorovajnov <wasilij@wasilij-Aspire-M3-481.(none)>
2014-01-22 10:13:57 +0100

    Some fixes needed for the end of the introduction. At the end of introduction the button to the next phase is the only one.

--- cg_implementierung/src/edu/kit/iks/Cryptographics/Caesar/Demonstration/IntroductionController.java
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/Caesar/Demonstration/IntroductionController.java
@@ -67,7 +67,8 @@ public class IntroductionController extends AbstractVisualizationController {
 	@Override
 	public String getHelp() {
 
-		return "Just click proceed till it works and see the magic.";
+		return "If you want to hear the awesome legend about caesar " +
+				"and obelix press the button below the text. Else try the next button!";
 	}
 
 	@Override
@@ -165,17 +166,18 @@ public class IntroductionController extends AbstractVisualizationController {
 
 		// set the alignment of the masterPlan image.
 		GridBagConstraints courierConstraint = new GridBagConstraints();
-		//courierConstraint.anchor = GridBagConstraints.PAGE_END;
-		//courierConstraint.weightx = 0.1;
+		// courierConstraint.anchor = GridBagConstraints.PAGE_END;
+		// courierConstraint.weightx = 0.1;
 		courierConstraint.gridx = 2;
 		courierConstraint.gridy = 0;
-		//courierConstraint.fill = GridBagConstraints.HORIZONTAL;
+		// courierConstraint.fill = GridBagConstraints.HORIZONTAL;
 
 		// take the image from the xml-resource.
 		this.getView().setCourier(
 				new ImageView(introResource.getChild("courierImage")
 						.getAttributeValue("path")));
-		this.getView().getAnimationContainer().add(this.getView().getCourier(), courierConstraint);
+		this.getView().getAnimationContainer()
+				.add(this.getView().getCourier(), courierConstraint);
 
 		// this.getView().firstAnimation();
 
@@ -194,17 +196,18 @@ public class IntroductionController extends AbstractVisualizationController {
 
 		// set the alignment of the masterPlan image.
 		GridBagConstraints obelixConstraint = new GridBagConstraints();
-		//obelixConstraint.anchor = GridBagConstraints.PAGE_END;
-		//obelixConstraint.weightx = 1.0;
+		// obelixConstraint.anchor = GridBagConstraints.PAGE_END;
+		// obelixConstraint.weightx = 1.0;
 		obelixConstraint.gridx = 3;
 		obelixConstraint.gridy = 0;
-		//obelixConstraint.fill = GridBagConstraints.VERTICAL;
+		// obelixConstraint.fill = GridBagConstraints.VERTICAL;
 
 		// take the image from the xml-resource.
 		this.getView().setObelix(
 				new ImageView(introResource.getChild("obelixImage")
 						.getAttributeValue("path")));
-		this.getView().getAnimationContainer().add(this.getView().getObelix(), obelixConstraint);
+		this.getView().getAnimationContainer()
+				.add(this.getView().getObelix(), obelixConstraint);
 
 		// this.getView().secondAnimation();
 	}
@@ -219,9 +222,7 @@ public class IntroductionController extends AbstractVisualizationController {
 				.setText(
 						this.getView().getExplanation().getText()
 								+ " Beim Lesen der Schriftrolle, die der Bote dabei hatte, erkannte Obelix Caesar's Pläne für <br> "
-								+ "den nächsten Angriff und die Gallier besiegten Caesar erneut. Caesar kochte vor Wut. <br>"
-								+ "Es ist zu beachten, dass dieser Text gewisse geschichtlichte Ungenauigkeiten auweist.<br>"
-								+ "Obelix konnte zum Beispiel kein Römisch!");
+								+ "den nächsten Angriff und die Gallier besiegten Caesar erneut. Caesar kochte vor Wut. <br>");
 
 		this.getView().validate();
 	}
@@ -230,7 +231,20 @@ public class IntroductionController extends AbstractVisualizationController {
 	 * 
 	 */
 	private void animationDone() {
-
+		this.getView()
+				.getExplanation()
+				.setText(
+						this.getView().getExplanation().getText()
+								+ "Es ist zu beachten, dass dieser Text gewisse geschichtlichte Ungenauigkeiten auweist.<br>"
+								+ "Obelix konnte zum Beispiel kein Römisch!");
+		GridBagLayout introLayout = (GridBagLayout) this.getView().getLayout();
+		GridBagConstraints finishConstraint = new GridBagConstraints();
+		finishConstraint.gridx = 1;
+		finishConstraint.gridy = 2;
+		introLayout.setConstraints(this.getView().getNextButton(),
+				finishConstraint);
+		this.getView().remove(this.getView().getProceed());
+		this.getView().setProceed(null);
 	}
 
 	/**
@@ -238,21 +252,6 @@ public class IntroductionController extends AbstractVisualizationController {
 	 */
 	public int getAnimationStep() {
 		return animationStep;
-	}
-
-	/**
-	 * @param animationStep
-	 *            the animationStep to set
-	 */
-	public void setAnimationStep(int animationStep) {
-		animationStep = animationStep;
-	}
-
-	/**
-	 * @return the caesarResource
-	 */
-	public Element getCaesarResource() {
-		return introResource;
 	}
 
 }
