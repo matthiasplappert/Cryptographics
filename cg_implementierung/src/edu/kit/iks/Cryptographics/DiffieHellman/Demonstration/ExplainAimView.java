ac864aaf93c977e369907568d7cd894d4084ed50
kai-rcc <kai-rcc@gmx.de>
2014-01-23 18:59:39 +0100

    draw the communication channel between alice, bob and eve

--- cg_implementierung/src/edu/kit/iks/Cryptographics/DiffieHellman/Demonstration/ExplainAimView.java
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/DiffieHellman/Demonstration/ExplainAimView.java
@@ -1,6 +1,8 @@
 package edu.kit.iks.Cryptographics.DiffieHellman.Demonstration;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
 import javax.swing.JLabel;
@@ -24,7 +26,15 @@ public class ExplainAimView extends VisualizationView {
 		this.setBackground(Color.WHITE);
 		this.aimExplain = new JLabel();
 		this.aimExplain.setText("Our aim is to exchange a secret key on a public channel");
-		this.add(aimExplain);
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		g.drawLine(450, 180, 800, 180);
+		g.drawLine(625, 120, 625, 180);
+		g.drawString("Alice", 400, 180);
+		g.drawString("Bob", 810, 180);
+		g.drawString("Eve", 610, 120);
 	}
 
 }
