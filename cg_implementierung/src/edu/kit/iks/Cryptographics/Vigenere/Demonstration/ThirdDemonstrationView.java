7aa505353388f574e8b33339efbe04249eff2387
AydinTekin <tekin.aydin.66@googlemail.com>
2014-02-06 21:14:06 +0100

    Fixed some unused imports

--- cg_implementierung/src/edu/kit/iks/Cryptographics/Vigenere/Demonstration/ThirdDemonstrationView.java
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/Vigenere/Demonstration/ThirdDemonstrationView.java
@@ -3,7 +3,6 @@ package edu.kit.iks.Cryptographics.Vigenere.Demonstration;
 import java.awt.Dimension;
 import java.awt.Font;
 
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
--- cg_implementierung/src/edu/kit/iks/Cryptographics/Vigenere/VigenereVisualizationInfo.java
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/Vigenere/VigenereVisualizationInfo.java
@@ -1,6 +1,5 @@
 package edu.kit.iks.Cryptographics.Vigenere;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
