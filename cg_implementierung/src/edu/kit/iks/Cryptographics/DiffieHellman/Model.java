f4edaee10142a716c0744c87fa035eb2a0695ffe
kai-rcc <kai-rcc@gmx.de>
2014-02-07 17:14:49 +0100

    undo singleton of dh model

--- cg_implementierung/src/edu/kit/iks/Cryptographics/DiffieHellman/Model.java
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/DiffieHellman/Model.java
@@ -3,7 +3,6 @@ package edu.kit.iks.Cryptographics.DiffieHellman;
 import java.awt.Color;
 
 public class Model {
-	private static Model instance;
 	
 	private Color alicePublicColor;
 	
@@ -19,20 +18,10 @@ public class Model {
 	
 	private Color bobMixedColor;
 	
-	/**
-	 * Singleton Class to share between different controller states
-	 */
-	private Model() {
+	public Model() {
 		
 	}
 	
-	public static Model getInstance() {
-		if(instance == null) {
-			instance = new Model();
-		}
-		return instance;
-	}
-
 	public Color getAlicePublicColor() {
 		return alicePublicColor;
 	}
