3b69ba8d13c3d61f742fabe4b94939e09bf4dca9
kai-rcc <kai-rcc@gmx.de>
2014-02-10 22:57:30 +0100

    add stub congrats view

--- cg_implementierung/src/edu/kit/iks/Cryptographics/DiffieHellman/Experiment/SendColorView.java
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/DiffieHellman/Experiment/SendColorView.java
@@ -1,13 +1,18 @@
 package edu.kit.iks.Cryptographics.DiffieHellman.Experiment;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class SendColorView extends JPanel {
 
 	private static final long serialVersionUID = 539729162359687203L;
 	
+	JLabel congrats = new JLabel("<html><div style=\"width:200px\">" +
+			"Congratulations</div></html>");
+	
 	public SendColorView() {
-		
+		add(congrats);
+		validate();
 	}
 
 }
