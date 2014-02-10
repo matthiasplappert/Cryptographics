356a282d1d621b491802fea17914c296b3defeaf
AydinTekin <tekin.aydin.66@googlemail.com>
2014-02-10 20:41:18 +0100

    Fixed gitignore

--- cg_implementierung/src/edu/kit/iks/Cryptographics/Vigenere/Explanation/FirstExplanationController.java
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/Vigenere/Explanation/FirstExplanationController.java
@@ -9,28 +9,31 @@ import edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo;
 
 public class FirstExplanationController extends AbstractVisualizationController {
 	
+	private int state;
+	
 	public FirstExplanationController(AbstractVisualizationInfo visualizationInfo) {
 		super(visualizationInfo);
 	}
 	
 	@Override
 	public FirstExplanationView getView() {
-		// TODO Auto-generated method stub
 		return (FirstExplanationView)this.view;
 	}
 	
 	@Override
 	public void loadView() {
-		// TODO Auto-generated method stub
+		this.state = 0;
 		this.view = new FirstExplanationView();
 		this.getView().getBackButton().addActionListener(new ActionListener() {
-			/*
-			 * @see java.awt.event.ActionListener#actionPerformed(java.awt
-			 * .event.ActionEvent)
-			 */
 			public void actionPerformed(ActionEvent event) {
 				VisualizationContainerController containerController = (VisualizationContainerController)getParentController();
 				containerController.presentPreviousVisualizationController();
+			}
+		});
+		this.getView().getKeyLengthButton().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				getView().setKeyLength("<html><div width=\"1200\">Length of key: 2</div></html>");
+				getView().getNextButton().setVisible(true);
 			}
 		});
 		this.getView().getNextButton().addActionListener(new ActionListener() {
