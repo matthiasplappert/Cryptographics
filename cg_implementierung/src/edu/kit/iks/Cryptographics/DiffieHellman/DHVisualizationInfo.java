be77f900c3b882607e94b0b7b308edf36f60f8a1
kai-rcc <kai-rcc@gmx.de>
2014-02-12 02:04:10 +0100

    add description to dhvisualizationinfo, parametrize timer of dh, and add comments

--- cg_implementierung/src/edu/kit/iks/Cryptographics/DiffieHellman/DHVisualizationInfo.java
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/DiffieHellman/DHVisualizationInfo.java
@@ -13,37 +13,78 @@ import edu.kit.iks.Cryptographics.DiffieHellman.Experiment.CongratsController;
 import edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo;
 import edu.kit.iks.CryptographicsLib.VisualizationDifficulty;
 
+/**
+ * The VisualizationInfo for Diffie-Hellman Key-Exchange Analogy
+ * @author kai
+ *
+ */
 public class DHVisualizationInfo extends AbstractVisualizationInfo {
+	
+	/**
+	 * Our description of the procedure
+	 */
+	private String description = "The infamous Diffie-Hellman Key-Exchange " +
+			"explained with a color-mixing analogy. Establish a shared secret, " +
+			"without an Eavesdropper getting the secret too";
+	
+	/*
+	 * (non-Javadoc)
+	 * @see edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo#getId()
+	 */
 	@Override
 	public String getId() {
 		return "diffiehellman";
 	}
 	
+	/*
+	 * (non-Javadoc)
+	 * @see edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo#getName()
+	 */
 	@Override
 	public String getName() {
 		return "Diffie-Hellman";
 	}
 	
+	/*
+	 * (non-Javadoc)
+	 * @see edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo#getDescription()
+	 */
 	@Override
 	public String getDescription() {
-		return "";
+		return description;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo#getTimelineOffset()
+	 */
 	@Override
 	public float getTimelineOffset() {
 		return 0.66f;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo#getDifficulty()
+	 */
 	@Override
 	public VisualizationDifficulty getDifficulty() {
 		return VisualizationDifficulty.EASY;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo#getYear()
+	 */
 	@Override
 	public int getYear() {
 		return 1976;
 	}
 	
+	/*
+	 * (non-Javadoc)
+	 * @see edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo#getControllerClasses()
+	 */
 	@Override
 	public List<Class> getControllerClasses() {
 		List<Class> controllerClasses = new ArrayList<Class>();
@@ -59,6 +100,10 @@ public class DHVisualizationInfo extends AbstractVisualizationInfo {
 		return controllerClasses;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo#getQRCodeContent()
+	 */
 	@Override
 	public String getQRCodeContent() {
 		// TODO Auto-generated method stub
