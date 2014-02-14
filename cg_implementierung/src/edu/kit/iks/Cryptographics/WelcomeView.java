16739c40d29ff8f60f5dd9f8d2257c0bc9fcbc31
Christian Dreher <CDreher3011@gmail.com>
2014-02-14 14:10:52 +0100

    Added descriptive welcome texts

--- cg_implementierung/src/edu/kit/iks/Cryptographics/WelcomeView.java
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/WelcomeView.java
@@ -1,5 +1,8 @@
 package edu.kit.iks.Cryptographics;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -24,7 +27,40 @@ public class WelcomeView extends JPanel {
 	 */
 	private static I18n i18n = Configuration.getInstance().getI18n(WelcomeView.class);
 	
+	/**
+	 * String to be displayed as header
+	 */
+	private String welcome = i18n.tr("Welcome");
+	
+	/**
+	 * String to be displayed as a description of how
+	 * to use the timeline
+	 */
+	private String description = i18n.tr("This is Cryptographics, a small tool "
+			+ "with the aim to teach the functionality of historic and common "
+			+ "cryptographic procedures. Below you can see a timeline with some "
+			+ "buttons each representing a procedures. Just click on one of the buttons "
+			+ "for a procedure you would like to know more about and a popup with further details "
+			+ "will explain what awaits you. If you want to know how it works, "
+			+ "press the 'Start' button.");
+	
 	public WelcomeView() {
-		this.add(new JLabel(i18n.tr("Welcome")));
+		super(new GridBagLayout());
+		
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		
+		this.add(new JLabel(this.wrapHtml("<h2>" + this.welcome + "</h2>")), gbc);
+		
+		gbc.gridy = 1;
+		
+		this.add(new JLabel(this.wrapHtml("<div width=\"500\">"
+				+ this.description
+				+ "</div>")), gbc);
+	}
+	
+	private String wrapHtml(String text) {
+		return "<html><body>" + text + "</body></html>";
 	}
 }
