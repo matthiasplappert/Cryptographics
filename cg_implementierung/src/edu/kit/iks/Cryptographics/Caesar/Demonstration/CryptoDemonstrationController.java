9e1f5231341906e7944a63558824896afe283b95
Wasilij Beskorovajnov <wasilij@wasilij-Aspire-M3-481.(none)>
2014-02-28 16:17:45 +0100

    Modified the keyboard in char Mode. Now Enter not needed there anymore.

--- cg_implementierung/src/edu/kit/iks/Cryptographics/Caesar/Demonstration/CryptoDemonstrationController.java
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/Caesar/Demonstration/CryptoDemonstrationController.java
@@ -137,8 +137,10 @@ public class CryptoDemonstrationController extends
 					@Override
 					public void actionPerformed(ActionEvent e) {
 						if (userOutput.isEditable()) {
-							// standart key for the caesar cipher. +3 when encrypting. -3 when
+							System.out.println("Action Performeddddddd");
+							// standard key for the caesar cipher. +3 when encrypting. -3 when
 							// decrypting.
+							String output = userOutput.getText();
 							if (CryptoDemonstrationController.this.getModel()
 									.enc(3, userOutput.getName())
 									.equals(userOutput.getText())) {
@@ -213,9 +215,7 @@ public class CryptoDemonstrationController extends
 								}
 							} else {
 								// User didn't encrypt correctly.
-								userOutput.setBorder(BorderFactory
-										.createLineBorder(Color.red));
-								userOutput.setText("");
+//								userOutput.setText("");
 								CryptoDemonstrationController.this
 										.getView()
 										.getExplanations()
