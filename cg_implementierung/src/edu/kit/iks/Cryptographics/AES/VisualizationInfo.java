0096f63277f9ad841dbddcdf6eae10a717ae1dd1
Matthias Plappert <matthiasplappert@me.com>
2014-03-02 19:14:13 +0100

    suppress warnings and add missing comments.

--- cg_implementierung/src/edu/kit/iks/Cryptographics/AES/VisualizationInfo.java
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/AES/VisualizationInfo.java
@@ -17,41 +17,66 @@ public class VisualizationInfo extends AbstractVisualizationInfo {
 	 */
 	private static I18n i18n = Configuration.getInstance().getI18n(VisualizationInfo.class);
 	
+	/*
+	 * @see edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo#getId()
+	 */
 	@Override
 	public String getId() {
 		return "aes";
 	}
 
+	/*
+	 * @see edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo#getName()
+	 */
 	@Override
 	public String getName() {
 		return i18n.tr("AES");
 	}
 
+	/*
+	 * @see edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo#getDescription()
+	 */
 	@Override
 	public String getDescription() {
 		return i18n.tr("Learn about one of the most important modern ciphers, the Advanced Encryption Standard!");
 	}
 
+	/*
+	 * @see edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo#getTimelineOffset()
+	 */
 	@Override
 	public float getTimelineOffset() {
 		return 0.93f;
 	}
-
+	
+	/*
+	 * @see edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo#getDifficulty()
+	 */
 	@Override
 	public VisualizationDifficulty getDifficulty() {
 		return VisualizationDifficulty.NOT_INTERACTIVE;
 	}
-
+	
+	/*
+	 * @see edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo#getYear()
+	 */
 	@Override
 	public int getYear() {
 		return 1998;
 	}
-
+	
+	/*
+	 * @see edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo#getQRCodeContent()
+	 */
 	@Override
 	public String getQRCodeContent() {
 		return i18n.tr("http://en.wikipedia.org/wiki/Advanced_Encryption_Standard");
 	}
-
+	
+	/*
+	 * @see edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo#getControllerClasses()
+	 */
+	@SuppressWarnings("rawtypes")
 	@Override
 	public List<Class> getControllerClasses() {
 		List<Class> controllerClasses = new ArrayList<Class>();
@@ -60,5 +85,4 @@ public class VisualizationInfo extends AbstractVisualizationInfo {
 		
 		return controllerClasses;
 	}
-
 }
--- cg_implementierung/src/edu/kit/iks/Cryptographics/OneTimePad/VisualizationInfo.java
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/OneTimePad/VisualizationInfo.java
@@ -17,41 +17,66 @@ public class VisualizationInfo extends AbstractVisualizationInfo {
 	 */
 	private static I18n i18n = Configuration.getInstance().getI18n(VisualizationInfo.class);
 	
+	/*
+	 * @see edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo#getId()
+	 */
 	@Override
 	public String getId() {
 		return "onetime-pad";
 	}
 
+	/*
+	 * @see edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo#getName()
+	 */
 	@Override
 	public String getName() {
 		return i18n.tr("One-Time Pad");
 	}
 
+	/*
+	 * @see edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo#getDescription()
+	 */
 	@Override
 	public String getDescription() {
 		return i18n.tr("Find out how you can encrypt something in such a way that it is impossible for someone else to crack it!");
 	}
 
+	/*
+	 * @see edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo#getTimelineOffset()
+	 */
 	@Override
 	public float getTimelineOffset() {
 		return 0.7f;
 	}
 
+	/*
+	 * @see edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo#getDifficulty()
+	 */
 	@Override
 	public VisualizationDifficulty getDifficulty() {
 		return VisualizationDifficulty.NOT_INTERACTIVE;
 	}
 
+	/*
+	 * @see edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo#getYear()
+	 */
 	@Override
 	public int getYear() {
 		return 1882;
 	}
-
+	
+	/*
+	 * @see edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo#getQRCodeContent()
+	 */
 	@Override
 	public String getQRCodeContent() {
 		return i18n.tr("http://en.wikipedia.org/wiki/One-time_pad");
 	}
-
+	
+	/*
+	 * @see edu.kit.iks.CryptographicsLib.AbstractVisualizationInfo#getControllerClasses()
+	 */
+	@SuppressWarnings("rawtypes")
 	@Override
 	public List<Class> getControllerClasses() {
 		List<Class> controllerClasses = new ArrayList<Class>();
