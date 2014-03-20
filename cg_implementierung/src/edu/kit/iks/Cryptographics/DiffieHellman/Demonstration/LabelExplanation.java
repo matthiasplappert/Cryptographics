97845cb24353609c5c586dd2d47e65b864c6fc99
kai-rcc <kai-rcc@gmx.de>
2014-03-20 15:09:04 +0100

    wrap newly added explanation string in i18n.tr()

--- cg_implementierung/src/edu/kit/iks/Cryptographics/DiffieHellman/Demonstration/LabelExplanation.java
+++ cg_implementierung/src/edu/kit/iks/Cryptographics/DiffieHellman/Demonstration/LabelExplanation.java
@@ -4,22 +4,37 @@ import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import org.xnap.commons.i18n.I18n;
+
+import edu.kit.iks.CryptographicsLib.Configuration;
+
 public class LabelExplanation extends JPanel {
 	
-	private String p = "p - The base color";
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8305176927157942920L;
 	
-	private String sA = "sA - Alice's secret color";
+	/**
+	 * Localization instance
+	 */
+	private static I18n i18n = Configuration.getInstance().getI18n(LabelExplanation.class);
+
+	private String p = i18n.tr("p - The base color");
 	
-	private String sB = "sB - Bob's secret color";
+	private String sA = i18n.tr("sA - Alice's secret color");
 	
-	private String AM = "AM - Mixture of sA and p";
+	private String sB = i18n.tr("sB - Bob's secret color");
 	
-	private String BM = "BM - Mixture of sB and p";
+	private String AM = i18n.tr("AM - Mixture of sA and p");
+	
+	private String BM = i18n.tr("BM - Mixture of sB and p");
 	
 	public LabelExplanation() {
 		super();
+		
+		
 		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-//		this
 		this.add(new JLabel(p));
 		this.add(new JLabel(sA));
 		this.add(new JLabel(sB));
