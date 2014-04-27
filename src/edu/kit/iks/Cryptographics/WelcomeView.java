9ad58d5c67a3544f1396ef1ecb170257499820e7
Christian Dreher <cdreher3011@gmail.com>
2014-04-27 22:45:32 +0200

    Removed long unnecessary texts and replaced them with some random changing code

--- src/edu/kit/iks/Cryptographics/WelcomeView.java
+++ src/edu/kit/iks/Cryptographics/WelcomeView.java
@@ -23,8 +23,11 @@ import java.awt.Font;
 import java.awt.FontFormatException;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
+import javax.swing.Timer;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -52,10 +55,99 @@ public class WelcomeView extends JPanel {
 	private static I18n i18n = Configuration.getInstance().getI18n(WelcomeView.class);
 	
 	/**
+	 * Generates the teaser code
+	 * 
+	 * @author Christian Dreher <uaeef@student.kit.edu>
+	 */
+	private class TeaserCode {
+		
+		/**
+		 * Text to be displayed
+		 */
+		private String text = "";
+		
+		/**
+		 * Length of the text
+		 */
+		private int textLength;
+		
+		/**
+		 * Constructor initializing a new random
+		 * text with given text length
+		 * 
+		 * @param textLength Length of the text
+		 */
+		public TeaserCode(int textLength) {
+			this.textLength = textLength;
+			
+			for (int i = 0; i < this.textLength; i++) {
+				text += this.newRandomChar();
+			}
+		}
+		
+		/**
+		 * Gets the text
+		 * 
+		 * @return The text
+		 */
+		public String getText() {
+			return this.text;
+		}
+		
+		/**
+		 * Tilts one random character in the text
+		 * 
+		 * @return Text with one tilted character
+		 */
+		public String tilt() {
+			char newChar = this.newRandomChar();
+			char[] textAsCharArray = this.text.toCharArray();
+			int charToTilt = this.randomInt(0, this.textLength - 1);
+			
+			textAsCharArray[charToTilt] = newChar;
+			this.text = String.valueOf(textAsCharArray);
+			
+			return this.text;
+		}
+		
+		private char newRandomChar() {
+			return (char) this.randomInt(33, 126);
+		}
+		
+		private int randomInt(int min, int max) {
+			return (min + (int) (Math.random() * ((max - min) + 1)));
+		}
+	}
+
+	/**
+	 * The teaser code to display
+	 */
+	private TeaserCode teaserCode = new TeaserCode(53);
+	
+	/**
+	 * JLabel of the teaser code. Needs to be explicitly set, since
+	 * it should be tilted by a timer
+	 */
+	private JLabel teaserCodeLabel = new JLabel(teaserCode.getText());
+	
+	/**
+	 * Timer to tilt the teaser code in given interval
+	 */
+	private Timer timer;
+	
+	/**
+	 * Interval in which the timer should tilt letters
+	 */
+	private int timerInterval = 10;
+	
+	/**
 	 * String to be displayed as header
 	 */
 	private String cryptographics = i18n.tr("Cryptographics");
 
+	/**
+	 * Constructor initializing the welcome view
+	 */
 	public WelcomeView() {
 		super(new GridBagLayout());
 		
@@ -67,18 +159,24 @@ public class WelcomeView extends JPanel {
 		gbc.gridy = 0;
 		
 		JLabel cryptographicsLabel = new JLabel(this.cryptographics);
-		cryptographicsLabel.setFont(this.prepareCryptographicsFont());
+		cryptographicsLabel.setFont(this.prepareFont(55));
+		this.teaserCodeLabel.setFont(new Font("Courier New", Font.PLAIN, 12));
 		this.add(cryptographicsLabel, gbc);
 		
 		gbc.gridy = 1;
 		
-		// TODO: remove when approved
-//		this.add(new JLabel(this.wrapHtml("<div width=\"1000\">"
-//				+ this.description
-//				+ "</div>")), gbc);
+		this.add(this.teaserCodeLabel, gbc);
+		
+		this.initTimer();
 	}
 	
-	private Font prepareCryptographicsFont() {
+	/**
+	 * Prepares the font for the cryptographics logo
+	 * 
+	 * @param size Fontsize
+	 * @return Initialized Font object
+	 */
+	private Font prepareFont(int size) {
 		
 		Font font = null;
 		
@@ -86,11 +184,27 @@ public class WelcomeView extends JPanel {
 			font = Font.createFont(Font.TRUETYPE_FONT,
 					ResourceUtil.getResourceAsInputStream("fonts/capture_smallz/Capsmall.ttf"));
 			
-			font = font.deriveFont(Font.PLAIN, 55);
+			font = font.deriveFont(Font.PLAIN, size);
 		} catch (FontFormatException | IOException e) {
 			Logger.error(e);
 		}
 		
 		return font;
+	}
+	
+	/**
+	 * Initialized the timer
+	 */
+	private void initTimer() {
+		this.timer = new Timer(this.timerInterval, new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent actionEvent) {
+				String newTeaserCode = WelcomeView.this.teaserCode.tilt();
+				WelcomeView.this.teaserCodeLabel.setText(newTeaserCode);
+			}
+		});
+		
+		this.timer.start();
 	}
 }
